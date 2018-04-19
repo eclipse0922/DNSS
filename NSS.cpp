6a2754b72769a9cfe2925915063e774564ebb2de
eclipse0922 <irocks0922@gmail.com>
2018-04-20 00:26:47 +0900

    first commit

---
+++ NSS.cpp
@@ -0,0 +1,417 @@
+
+#include <NSS.h>
+#include <glm/gtx/norm.hpp>
+#include <glm/glm.hpp>
+#include <iostream>
+#include <ppl.h>
+
+void NSS::normalSpaceSampling(const float &sample_rate, std::vector<glm::fvec3> &sampled_vertices, std::vector<glm::fvec3> &sampled_normals)
+{
+	std::vector< std::vector<int> > normbuckets;
+	sort_into_buckets(m_original_normals, normbuckets);
+	int ndesired = int(ceil(sample_rate * m_original_vertices.size()));
+	sampled_vertices.clear();  sampled_normals.clear();
+	while (sampled_vertices.size() < ndesired)
+	{
+		for (int i = 0; i < normbuckets.size(); i++)
+		{
+			if (!normbuckets[i].empty())
+			{
+				int ind = normbuckets[i].back();
+				sampled_vertices.emplace_back(m_original_vertices[ind]);
+				sampled_normals.emplace_back(m_original_normals[ind]);
+				normbuckets[i].pop_back();
+			}
+		}
+	}
+}
+
+void NSS::sort_into_buckets(const std::vector<glm::fvec3> &n, std::vector< std::vector<int>> &normbuckets)
+{
+	const int Q = 4;
+	const float Qsqrt1_2 = 2.8284f;
+	normbuckets.resize(3 * Q*Q);
+	for (int i = 0; i < n.size(); i++)
+	{
+		const float *N = &n[i][0];
+
+		float ax = fabs(N[0]), ay = fabs(N[1]), az = fabs(N[2]);
+		int A;
+		float u, v;
+		if (ax > ay)
+		{
+			if (ax > az)
+			{
+				A = 0;
+				u = (N[0] > 0) ? N[1] : -N[1];
+				v = (N[0] > 0) ? N[2] : -N[2];
+			}
+			else {
+				A = 2;
+				u = (N[2] > 0) ? N[0] : -N[0];
+				v = (N[2] > 0) ? N[1] : -N[1];
+			}
+		}
+		else
+		{
+			if (ay > az)
+			{
+				A = 1;
+				u = (N[1] > 0) ? N[2] : -N[2];
+				v = (N[1] > 0) ? N[0] : -N[0];
+			}
+			else
+			{
+				A = 2;
+				u = (N[2] > 0) ? N[0] : -N[0];
+				v = (N[2] > 0) ? N[1] : -N[1];
+			}
+		}
+		int U = int(u * Qsqrt1_2) + (Q / 2);
+		int V = int(v * Qsqrt1_2) + (Q / 2);
+		normbuckets[((A * Q) + U) * Q + V].push_back(i);
+	}
+	for (int bucket = 0; bucket < normbuckets.size(); bucket++)
+		std::random_shuffle(normbuckets[bucket].begin(), normbuckets[bucket].end());
+}
+
+void DNSS::dualNormalSpaceSampling(const float &sample_rate, std::vector<glm::fvec3> &sampled_vertices, std::vector<glm::fvec3> &sampled_normals)
+{
+
+	//Timer timer;
+//	timer.start();
+	computeCentroidandNormalize();
+	computeRotationalNormals();
+	computeRotationalReturn();
+
+	sortIntoBucket();
+	initBucketB();
+	int ndesired = int(ceil(sample_rate * m_vertices_original.size()));
+
+	sampled_vertices.clear();  sampled_normals.clear();
+	sampled_vertices.reserve(ndesired);  sampled_normals.reserve(ndesired);
+
+	while (sampled_vertices.size() < ndesired)
+	{
+		int pid = pickPoint();
+
+		sampled_vertices.emplace_back(m_vertices_original[pid]);
+		sampled_normals.emplace_back(m_normals_original[pid]);
+		//updateBucketOrder();
+	}
+	sampled_vertices.shrink_to_fit();  sampled_normals.shrink_to_fit();
+	//	timer.stop();
+	//	std::wcout << L"DNSS sampling time : " << timer.peek_msec() << L" (ms)" << std::endl;
+}
+
+void DNSS::computeRotationalReturn()
+{
+	int nSize = static_cast<int>(m_vertices_normalized.size());
+	m_rotationalReturns.clear();
+	m_rotationalReturns.resize(nSize);
+
+	Concurrency::parallel_for(0, nSize, [&](int idx)
+	{
+		float dot_pn = glm::dot(glm::normalize(m_vertices_normalized[idx]), m_normals_original[idx]);
+		float beta = acos(dot_pn);// / (glm::l2Norm(m_vertices_normalized[idx])*glm::l2Norm(m_normals_original[idx])));
+		float p_abs = glm::l2Norm(m_vertices_normalized[idx]);
+		float pp_abs = 2 * p_abs *sin(m_thetaForReturn / 2);
+
+		float pq_abs_positive = pp_abs * cos(beta - m_thetaForReturn / 2);
+		float pq_abs_negative = pp_abs * cos(-beta - m_thetaForReturn / 2);
+		float sin_beta = sin(beta);
+		float cos_beta = cos(beta);
+
+		float atanbeta_positive = atan((pq_abs_positive*sin_beta) / (p_abs - pq_abs_positive * cos_beta));
+		float gamma_positive = m_thetaForReturn - atanbeta_positive;
+
+		float atanbeta_negative = atan((pq_abs_negative*(-sin_beta)) / (p_abs - pq_abs_negative * cos_beta));
+		float gamma_negative = m_thetaForReturn - atanbeta_negative;
+
+		float rotationalReturnPositive = p_abs * gamma_positive / m_thetaForReturn;
+		float rotationalReturnNegative = p_abs * gamma_negative / m_thetaForReturn;
+
+		float RR = fmax(rotationalReturnPositive, rotationalReturnNegative);
+		m_rotationalReturns[idx] = RR;
+	}
+	);
+}
+void DNSS::initBucketB()
+{
+	m_bucketList_T.clear();
+	m_bucketList_R.clear();
+	m_bucketList_T.resize(m_bucketTranslation.size());
+	m_bucketList_R.resize(m_bucketRotation.size());
+	const int bucketsize_R = static_cast<int>(m_bucketRotation.size());
+	const int bucketsize_T = static_cast<int>(m_bucketTranslation.size());
+	for (int idx = 0; idx < bucketsize_R; idx++)
+	{
+		m_bucketList_R[idx].bucketIndex = idx;
+		//	m_bucketRotation[idx].clear();
+		if (m_bucketRotation[idx].empty())
+			m_bucketList_R[idx].constraints = FLT_MAX;
+		else
+			m_bucketList_R[idx].constraints = 0.0f;
+	}
+
+	for (int idx = 0; idx < bucketsize_T; idx++)
+	{
+		m_bucketList_T[idx].bucketIndex = idx;
+		//	m_bucketTranslation[idx].clear();
+		if (m_bucketTranslation[idx].empty())
+			m_bucketList_T[idx].constraints = FLT_MAX;
+		else
+			m_bucketList_T[idx].constraints = 0.0f;
+	}
+	typeBucket r = typeBucket::Rotation, t = typeBucket::Translation;
+	updateBucketOrder(r);
+	updateBucketOrder(t);
+}
+
+void DNSS::sortIntoBucket()
+{
+	int nSizePoints = static_cast<int>(m_vertices_normalized.size());
+	std::wcout << L"niszeP_points : " << nSizePoints << std::endl;
+	int nSizeBucketR = m_bucketsizeR_azimuth * m_bucketsizeR_polar;
+	int nSizeBucketT = m_bucketsizeT_azimuth * m_bucketsizeT_polar;
+	// concurrent vector에 넣고 정렬해서 멤버 변수에 다시 넣는다.
+	// 이유: concurrent_vector pop_back 사용불가
+	std::vector<concurrency::concurrent_vector<std::pair<int, float>>> bucketRotation;
+	std::vector<concurrency::concurrent_vector<int>> bucketTranslation;
+	m_vecForBIdx.clear();
+	m_vecForBIdx.resize(nSizePoints);
+
+	bucketRotation.clear();
+	bucketRotation.resize(nSizeBucketR);
+	bucketTranslation.clear();
+	bucketTranslation.resize(nSizeBucketT);
+
+	m_bucketRotation.clear();
+	m_bucketRotation.resize(nSizeBucketR);
+	m_bucketTranslation.clear();
+	m_bucketTranslation.resize(nSizeBucketT);
+
+
+	//버켓 공간 확보 - 버켓에 완전히 균일하게 들어가는 것이 아니고 한쪽에 몰릴수도 있어, 버켓 사이즈로 나누지 않고 10으로 나눔(임의로 정한 수)
+	Concurrency::parallel_for(0, nSizeBucketR, [&](int idx)
+	{
+		bucketRotation[idx].reserve(nSizePoints / 10);
+	});
+	Concurrency::parallel_for(0, nSizeBucketT, [&](int idx)
+	{
+		bucketTranslation[idx].reserve(nSizePoints / 10);
+	});
+
+	Concurrency::parallel_for(0, nSizePoints, [&](int idx)
+	{
+		float coord_R_azimuth = 0;
+		float coord_R_polar = 0;
+		computeSphericalCoordinate(glm::normalize(m_normals_rotational[idx]), coord_R_azimuth, coord_R_polar);
+		int index_R_azimuth = static_cast<int>(std::floorf(fabs(coord_R_azimuth) / m_thetaForSort));
+		int index_R_polar = static_cast<int>(std::floorf(coord_R_polar / m_thetaForSort));
+		int index_R = index_R_azimuth * m_bucketsizeR_polar + index_R_polar;
+
+		bucketRotation[index_R].push_back(std::make_pair(idx, m_rotationalReturns[idx]));
+
+		float coord_T_azimuth = 0;
+		float coord_T_polar = 0;
+		computeSphericalCoordinate(glm::normalize(m_normals_original[idx]), coord_T_azimuth, coord_T_polar);
+		int index_T_azimuth = static_cast<int>(std::floorf((coord_T_azimuth + m_pi_degree) / m_thetaForSort));
+		int index_T_polar = static_cast<int>(std::floorf(coord_T_polar / m_thetaForSort));
+		int index_T = index_T_azimuth * m_bucketsizeT_polar + index_T_polar;
+
+		bucketTranslation[index_T].push_back(idx);
+
+		m_vecForBIdx[idx] = std::make_pair(index_R, index_T);
+	}
+	);
+
+	Concurrency::parallel_for(0, nSizeBucketR, [&](int idx)
+	{
+		bucketRotation[idx].shrink_to_fit();
+		m_bucketRotation[idx].resize(bucketRotation[idx].size());
+		std::move(bucketRotation[idx].begin(), bucketRotation[idx].end(), m_bucketRotation[idx].begin());
+
+	});
+	Concurrency::parallel_for(0, nSizeBucketT, [&](int idx)
+	{
+		bucketTranslation[idx].shrink_to_fit();
+		m_bucketTranslation[idx].resize(bucketTranslation[idx].size());
+		std::move(bucketTranslation[idx].begin(), bucketTranslation[idx].end(), m_bucketTranslation[idx].begin());
+	});
+
+
+	Concurrency::parallel_for(0, nSizeBucketR, [&](int idx)
+	{
+		//pop_back만 가능하므로 return값이 큰 것이 뒤로 가도록 정렬
+		std::sort(m_bucketRotation[idx].begin(), m_bucketRotation[idx].end(),
+			[](const std::pair<int, float>& lhs, const std::pair<int, float>& rhs) {
+			return lhs.second < rhs.second;
+		});
+	});
+
+	Concurrency::parallel_for(0, nSizeBucketT, [&](int idx)
+	{
+		std::random_shuffle(m_bucketTranslation[idx].begin(), m_bucketTranslation[idx].end());
+	});
+}
+
+void DNSS::computeRotationalNormals()
+{
+	int nSize = static_cast<int>(m_normals_original.size());
+	m_normals_rotational.resize(m_normals_original.size());
+	Concurrency::parallel_for(0, nSize, [&](int idx)
+	{
+		m_normals_rotational[idx] = glm::cross(m_vertices_normalized[idx], m_normals_original[idx]);
+	});
+}
+
+void DNSS::computeCentroidandNormalize()
+{
+	Eigen::Matrix3Xf vertices = Eigen::Map<const Eigen::Matrix3Xf>(&m_vertices_original[0].x, 3, m_vertices_original.size());
+	Eigen::Vector3f centroid_eigen = vertices.rowwise().mean();
+	glm::fvec3 centroid = (glm::fvec3&)(*centroid_eigen.data());
+
+	int nSize = static_cast<int>(m_vertices_original.size());
+	m_vertices_normalized.clear();
+	m_vertices_normalized.resize(nSize);
+	Concurrency::parallel_for(0, nSize, [&](int idx)
+	{
+		m_vertices_normalized[idx] = m_vertices_original[idx] - centroid;
+	});
+	Eigen::Matrix3Xf vertices_moved = Eigen::Map<const Eigen::Matrix3Xf>(&m_vertices_normalized[0].x, 3, m_vertices_normalized.size());
+	//normalize factor
+	Eigen::Vector3f centroid_eigen_moved = vertices_moved.rowwise().maxCoeff();
+	float Lmax = vertices_moved.colwise().norm().maxCoeff();
+	float L_inverse = 1 / Lmax;
+	Concurrency::parallel_for(0, nSize, [&](int idx)
+	{
+		m_vertices_normalized[idx] = m_vertices_normalized[idx] * L_inverse;
+	});
+}
+
+int DNSS::pickPoint()
+{
+	int bid_top;
+	typeBucket bType_top, bType_another;
+
+	if (m_bucketList_R.front().constraints <= m_bucketList_T.front().constraints)
+	{
+		bid_top = m_bucketList_R.front().bucketIndex;
+		bType_top = typeBucket::Rotation;
+		bType_another = typeBucket::Translation;
+	}
+	else
+	{
+		bid_top = m_bucketList_T.front().bucketIndex;
+		bType_top = typeBucket::Translation;
+		bType_another = typeBucket::Rotation;
+	}
+	int pid = -1;
+	switch (bType_top)
+	{
+	case typeBucket::Rotation:
+	{
+		if (m_bucketRotation[bid_top].empty())
+		{
+			std::wcout << L"bucket id : " << bid_top <<
+				" and bucket size : " << m_bucketRotation[bid_top].size() << std::endl;
+			std::wcout << L"constraints : " << m_bucketList_R[bid_top].constraints << std::endl;
+
+		}
+
+		pid = m_bucketRotation[bid_top].back().first;
+
+		m_bucketList_R.front().constraints += m_bucketRotation[bid_top].back().second;
+		m_bucketRotation[bid_top].pop_back();
+
+		int bid_T = m_vecForBIdx[pid].second;
+
+		if (!m_bucketTranslation[bid_T].empty())
+		{
+			m_bucketTranslation[bid_T].erase(std::remove_if(m_bucketTranslation[bid_T].begin(), m_bucketTranslation[bid_T].end(),
+				[&pid](const int &elem) { return elem == pid; }),
+				m_bucketTranslation[bid_T].end());
+		}
+		if (m_bucketTranslation[bid_T].empty())
+		{
+			auto it = std::find_if(m_bucketList_T.begin(), m_bucketList_T.end(),
+				[bid_T](const structBucket & element) { return element.bucketIndex == bid_T; });
+			it->constraints = FLT_MAX;
+			updateBucketOrder(bType_another);
+		}
+
+		if (m_bucketRotation[bid_top].empty())
+		{
+			m_bucketList_R.front().constraints = FLT_MAX;
+		}
+		updateBucketOrder(bType_top);
+		break;
+	}
+	case typeBucket::Translation:
+	{
+		if (m_bucketTranslation[bid_top].empty())
+		{
+			std::wcout << L"bucket id : " << bid_top <<
+				" and bucket size : " << m_bucketTranslation[bid_top].size() << std::endl;
+			std::wcout << L"constraints : " << m_bucketList_T[bid_top].constraints << std::endl;
+		}
+		pid = m_bucketTranslation[bid_top].back();
+
+
+		m_bucketList_T.front().constraints += 1.0f;
+		m_bucketTranslation[bid_top].pop_back();
+
+		int bid_R = m_vecForBIdx[pid].first;
+
+		if (!m_bucketRotation[bid_R].empty())
+		{
+			m_bucketRotation[bid_R].erase(std::remove_if(m_bucketRotation[bid_R].begin(), m_bucketRotation[bid_R].end(),
+				[&pid](const std::pair<int, float> &elem) { return elem.first == pid; }),
+				m_bucketRotation[bid_R].end());
+		}
+		if (m_bucketRotation[bid_R].empty())
+		{
+			auto it = std::find_if(m_bucketList_R.begin(), m_bucketList_R.end(),
+				[bid_R](const structBucket & element) { return element.bucketIndex == bid_R; });
+			it->constraints = FLT_MAX;
+			updateBucketOrder(bType_another);
+		}
+		if (m_bucketTranslation[bid_top].empty())
+		{
+			m_bucketList_T.front().constraints = FLT_MAX;
+		}
+		updateBucketOrder(bType_top);
+		break;
+	}
+	}
+	return pid;
+}
+void DNSS::updateBucketOrder(typeBucket &bType)
+{
+	switch (bType)
+	{
+	case typeBucket::Rotation:
+		std::sort(m_bucketList_R.begin(), m_bucketList_R.end(),
+			[](const structBucket& lhs, const structBucket & rhs) {
+			return lhs.constraints < rhs.constraints;
+		});
+		break;
+	case typeBucket::Translation:
+		std::sort(m_bucketList_T.begin(), m_bucketList_T.end(),
+			[](const structBucket& lhs, const structBucket & rhs) {
+			return lhs.constraints < rhs.constraints;
+		});
+		break;
+	}
+
+}
+
+void DNSS::computeSphericalCoordinate(const glm::fvec3 &normal, float &coordinates_azimuth, float &coordinates_polar)
+{
+	float radian_azimuth = atan2(normal.y, normal.x);
+	float radian_polar = acos(normal.z);
+	coordinates_azimuth = radian_azimuth * m_pi_radian / m_pi2_degree;
+	coordinates_polar = radian_polar * m_pi_radian / m_pi2_degree;
+
+}
---
+++ NSS.h
@@ -0,0 +1,166 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <algorithm>
+#include <vector>
+#include <Eigen/Dense>
+#include <Eigen/Geometry>
+#include <Eigen/Core>
+#include <concurrent_queue.h>
+#include <concurrent_vector.h>
+#include <concurrent_unordered_map.h>
+
+/*
+ * normal space sampling algorithm
+*
+ *
+ * original source code: https://github.com/kyzyx/scanalyze/
+ *Scanalyze (version 1.0) license information
+ -------------------------------------------
+
+ Scanalyze is copyright (c) 2002 the Board of Trustees of The Leland
+ Stanford Junior University. All rights reserved.
+
+ This software is covered by the Stanford Computer Graphics Laboratory's
+ General Software License. This license is royalty-free, nonexclusive,
+ and nontransferable.  The terms and conditions of this license are
+ viewable at this URL:
+
+ http://graphics.stanford.edu/software/license.html
+
+
+ The scanalyze software also builds upon a number of 3rd party
+ software components, some of which may be copyrighted:
+
+ 1. Togl
+ 2. Tcl/Tk
+ 3. STL
+ 4. Template Numerical Toolkit (TNT)
+ 5. Janne Heikkila's Camera Calibration Toolbox
+ 6. Graphics Gems
+ 7. Miscellaneous SGI code
+
+ Further information about each of these software components and
+ their terms of usage is included below:
+
+ ----------------------------------------------------------------------
+ *
+ **/
+class NSS
+{
+public:
+
+	/** \brief normal space sampling 수행 함수
+	* \param[in] sample_rate 샘플링레이트  최대 1.0  비율 곱해서 개수 채워질때까지 반복수행
+	* \param[out] sampled_vertices 샘플링한 포인트 데이터
+	* \param[out] sampled_normals 샘플링한 노멀 데이터
+	*/
+	void normalSpaceSampling(const float &sample_rate, std::vector<glm::fvec3> &sampled_vertices, std::vector<glm::fvec3> &sampled_normals);
+
+	/** \brief sampling하기위한 원본 데이터 지정위한 함수
+	* \param[in] pointData 포인트 데이터
+	* \param[in] normalData 노멀 데이터
+	*/
+	void setInputCloud(const std::vector<glm::fvec3> original_vertices, const std::vector<glm::fvec3> original_normals)
+	{
+		m_original_vertices = original_vertices;
+		m_original_normals = original_normals;
+	}
+private:
+	/** \brief normal 값을 기준으로 인덱스 생성한 뒤 인덱스 값을 보고 버켓에 저장
+	* \param[in] const std::vector<glm::fvec3> &n 일련의 노말(normal) 값
+	* \param[out] std::vector< std::vector<int>> 인덱스가 들어간 버켓
+	*/
+	void sort_into_buckets(const std::vector<glm::fvec3> &n, std::vector< std::vector<int>> &normbuckets);
+
+private:
+	std::vector<glm::fvec3> m_original_vertices;
+	std::vector<glm::fvec3> m_original_normals;
+};
+
+/*
+ * Dual Normal Space Sampling
+ * Kwok, Tsz-Ho. "DNSS: Dual-Normal-Space Sampling for 3-D ICP Registration."
+ * IEEE Transactions on Automation Science and Engineering (2018).
+ * 기존 NSS에 rotation 정보도 포함시켜서 샘플링
+ * adding Rotational Information to NSS
+ * 직접 구현
+ * 작성자:  seweon.jeon
+ * 2018.04.09
+ **/
+
+class DNSS
+{
+public:
+	void setInputCloud(const std::vector<glm::fvec3> original_vertices, const std::vector<glm::fvec3> original_normals)
+	{
+		m_vertices_original = original_vertices;
+		m_normals_original = original_normals;
+	}
+	void dualNormalSpaceSampling(const float &sample_rate, std::vector<glm::fvec3> &sampled_vertices, std::vector<glm::fvec3> &sampled_normals);
+private:
+	struct structBucket
+	{
+		int bucketIndex = 0;
+		float constraints = 0;
+	};
+
+	enum class typeBucket :int
+	{
+		Rotation = 0,
+		Translation,
+	};
+
+	void computeRotationalReturn();
+	void initBucketB();
+	void sortIntoBucket();
+	void computeRotationalNormals();
+	void computeCentroidandNormalize();
+
+	int pickPoint();
+
+	void updateBucketOrder(typeBucket &bType);
+	/**
+	 * \brief Normal(nx,ny,nz) 받아서 azimuth,polar 값계산
+	 *   논문과 iso rule 각각에서 azimuth, polar angle  지칭하는 것이 달라 iso rule에 맞춤
+	 *     i followed ISO rule for spherical coorinate system naming
+	 *  iso rule    theta -  polar angle   (0~ pi) phi - azimuth angle( 0~2pi)
+	 *  논문         theta -  azimuth angle (0~2pi) phi - polar angle  ( 0~pi)
+	 *  pi== 180; 2pi ==360;
+	 * \param[in] glm::fvec3 normal 값
+	 * \param[out] float coordinates_theta
+	 * \param[out] float coordinates_phi
+	 */
+	void computeSphericalCoordinate(const glm::fvec3 &normal, float &coordinates_azimuth, float &coordinates_polar);
+
+private:
+	std::vector<structBucket> m_bucketList_R;
+	std::vector<structBucket> m_bucketList_T;
+	std::vector<std::pair<int, int>> m_vecForBIdx;
+	std::vector<std::vector<std::pair<int, float>>> m_bucketRotation;
+	std::vector<std::vector<int>> m_bucketTranslation;
+	std::vector<glm::fvec3> m_vertices_original;
+	std::vector<glm::fvec3> m_normals_original;
+	std::vector<glm::fvec3> m_vertices_normalized;
+	std::vector<glm::fvec3> m_normals_rotational;
+	std::vector<float> m_rotationalReturns;
+
+	/*
+	*
+	 *   \note 논문과 iso rule 각각에서 azimuth, polar angle 지칭하는 것이 달라 iso rule에 맞춤
+	 *      i followed ISO rule for spherical coorinate system naming   
+	 *		iso rule    theta -  polar angle   (0~180) phi - azimuth angle( 0~360)
+	 *		paper       theta -  azimuth angle (0~360) phi - polar angle( 0~180)
+	 */
+	static const int m_bucketsizeT_azimuth = 12;
+	static const int m_bucketsizeT_polar = 6;
+	static const int m_bucketsizeR_azimuth = 6;
+	static const int m_bucketsizeR_polar = 6;
+
+	const float m_pi_degree = 180.0f;
+	const float m_pi_radian = 3.141592f;
+	const float m_pi2_degree = 360.0f;
+	const float m_thetaForSort = m_pi_degree / 6;
+	const float m_thetaForReturn = m_pi_degree / 4;
+};
+
